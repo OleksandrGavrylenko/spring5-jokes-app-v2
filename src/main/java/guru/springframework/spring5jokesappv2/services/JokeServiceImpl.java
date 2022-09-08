8e4d1adde0d451109563f682a64fa5339ca278b8
Oleksandr.Gavrylenko <Oleksandr.Gavrylenko@tietoevry.com>
2022-09-09 00:45:03 +0300

    Refactored and banner added

--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -4,11 +4,11 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokesServiceImpl implements JokesService {
+public class JokeServiceImpl implements JokeService {
 
     final private ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
         this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
